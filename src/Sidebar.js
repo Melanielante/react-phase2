39ed2b461a90a9a9c1d7cde3b73855947a4f3751
Oliver Lomanat <lomanatoliver@gmail.com>
2025-07-17 14:56:18 +0300

    Adds Greetings

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,8 @@ export default function Sidebar() {
     return (
         <div>
             <h1>Sidebar</h1>
+            <p>Hello, Guys.</p>
+            <p>Hit me up!</p>
         </div>
     )
 }
